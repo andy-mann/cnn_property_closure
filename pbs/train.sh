e5291fb82224347f315beb637973a081e8bfbd89
Andrew Mcdonald Mann <amann37@login-hive1.pace.gatech.edu>
2021-11-09 16:51:38 -0500

    added logs

--- pbs/train.sh
+++ pbs/train.sh
@@ -1,10 +1,10 @@
-#PBS -N gpuTest       		 # job name
-#PBS -l nodes=1:ppn=1:gpus=1     # number of nodes, cores per node, and gpus required
-#PBS -l pmem=32gb                # memory per core
+#PBS -N NN_Training    		 # job name
+#PBS -l nodes=1:ppn=2:gpus=1     # number of nodes, cores per node, and gpus required
+#PBS -l pmem=64gb                # memory per core
 #PBS -l walltime=4:00:00         # duration of the job (ex: 15 min)
 #PBS -q hive-gpu-short         	 # queue name (where job is submitted)
 #PBS -j oe                       # combine output and error messages into 1 file
-#PBS -o log.out	         # output file name
+#PBS -o ${PBS_JOBID}_log.out      # output file name
 
 homedir="/storage/home/hhive1/amann37"
 working_dir=${homedir}"/code/materials_informatics"
